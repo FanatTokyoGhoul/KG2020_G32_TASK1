92ac56a219b91931679b8eae21c1e53665fd2520
FanatTokyoGhoul <nayzer322@gmail.com>
2020-09-18 19:57:09 +0300

    Replaced the normal output to the screen with buffer output to the screen and moved DrawPanel to a separate class.

---
+++ KG_2020_TASK1/src/DrawPanel.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+public class DrawPanel extends JPanel {
+
+    /**This is DrawPanel. Here I draw a picture in the BufferedImage in advance and then display it on the screen.**/
+
+    private List<GraphicsObject> listObject;
+
+    public DrawPanel(List<GraphicsObject> listObject) {
+        this.listObject = listObject;
+    }
+
+    @Override
+    public void paint(Graphics graphics){
+        BufferedImage bufferedImage = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D gr = bufferedImage.createGraphics();
+        for(GraphicsObject object: listObject){
+            object.draw(gr);
+        }
+        graphics.drawImage(bufferedImage,0,0, null);
+    }
+}
--- KG_2020_TASK1/src/MainFrame.java
+++ KG_2020_TASK1/src/MainFrame.java
@@ -1,5 +1,6 @@
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,45 +9,28 @@ public class MainFrame extends JFrame {
 
     private List<GraphicsObject> listObject = new ArrayList<>();
 
-    public MainFrame(){
+    private void creatingAPicture() {
+        listObject.add(new Background(0, 0, 1000, 350, new Color(0, 255, 255)));
+        listObject.add(new Background(0, 350, 1000, 350, new Color(205, 133, 63)));
+        listObject.add(new Cloud(250, 150, 250));
+        listObject.add(new Cloud(550, 100, 250));
+        listObject.add(new Sun(100, new Color(255, 255, 0), 900, 125, 25));
+        listObject.add(new Lake(500, 500, 300));
+        listObject.add(new Tree(500, 400, 150));
+        listObject.add(new Tree(175, 500, 215));
+        listObject.add(new Tree(900, 450, 200));
+        listObject.add(new Tree(700, 650, 400));
+        listObject.add(new Tree(300, 550, 300));
+        listObject.add(new Tree(50, 650, 400));
+        listObject.add(new TextTable(450, 550, 50, "Не мусорить!"));
+    }
+
+    public MainFrame() {
         super("Task 1");
         DrawPanel panel = new DrawPanel(listObject);
         setMinimumSize(new Dimension(1000, 739));
         setContentPane(panel);
         creatingAPicture();
         setVisible(true);
-    }
-
-    private void creatingAPicture(){
-        listObject.add(new Background(0,0, 1000,350, new Color(0,255,255)));
-        listObject.add(new Background(0,350, 1000,350, new Color(205,133,63)));
-        listObject.add(new Cloud(250, 150, 250));
-        listObject.add(new Cloud(550, 100, 250));
-        listObject.add(new Sun(100, new Color(255,255,0),900, 125, 25));
-        listObject.add(new Lake(500,500, 300));
-        listObject.add(new Tree(500,400,150));
-        listObject.add(new Tree(175,500,215));
-        listObject.add(new Tree(900,450,200));
-        listObject.add(new Tree(700,650,400));
-        listObject.add(new Tree(300,550,300));
-        listObject.add(new Tree(50,650,400));
-        listObject.add(new TextTable(450,550,50, "Не мусорить!"));
-    }
-
-
-    static class DrawPanel extends JPanel {
-        private List<GraphicsObject> listObject;
-
-        public DrawPanel(List<GraphicsObject> listObject) {
-            this.listObject = listObject;
-        }
-
-        @Override
-        public void paint(Graphics graphics){
-            Graphics2D gr = (Graphics2D)graphics;
-            for(GraphicsObject object: listObject){
-                object.draw(gr);
-            }
-        }
     }
 }
